ab13c5998cbaa0df478db3009bd622687d9ddc3f
adityachintala <adityachintala27@gmail.com>
2023-08-17 19:48:02 +0530

    Completed number management system

--- number-management-microservice/index.js
+++ number-management-microservice/index.js
@@ -4,24 +4,49 @@ const app = express();
 const port = 8008;
 
 app.get('/numbers', async (req, res) => {
-    const urls = req.query.url;
-    console.log(urls);
+    let urls = req.query.url;
+    if (typeof urls === 'string') {
+        urls = [urls];
+    }
     const numbers = await getNumbers(urls);
     res.send({ numbers });
-    });
+});
 
+const Validator = (obj) => {
+    if ((typeof obj !== 'object' || obj === null) || !Array.isArray(obj.numbers)) {
+        return false;
+    }
+    for (const num of obj.numbers) {
+        if (typeof num !== 'number' || !Number.isInteger(num)) {
+            return false;
+        }
+    }
+    return true;
+}
 
 async function getNumbers(urls) {
     const promises = urls.map(async (url) => {
         try {
             const response = await axios.get(url);
-            return response.data.numbers;
+            // here we can check if the response is valid JSON
+            if (Validator(response.data)) {
+                return response.data.numbers;
+            }
+            else{
+                return [];
+            }
         } catch (error) {
-            console.log(error);
+            return [];
         }
     });
-    const numbers = await Promise.all(promises);
-    return numbers.flat().sort((a, b) => a - b);
+
+    let numbers = await Promise.all(promises);
+
+    // console.log(numbers);
+
+    // convert array of arrays into one array
+    numbers = numbers.flat();
+    return [...new Set(numbers)].sort((a, b) => a - b);
 }
 
 app.listen(port, () => {
